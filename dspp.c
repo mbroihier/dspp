3e30523052911af0585be30a651745a45773c9d6
broihier <broihier@Zeta.local>
2019-05-08 17:54:43 -0500

    add shift_frequency_cc

--- dspp.c
+++ dspp.c
@@ -28,6 +28,7 @@
 int convert_byteLE_int16();
 int convert_aByte_f();
 int convert_aUnsignedByte_f();
+int shift_frequency_cc(float cyclesPerSample);
 
 
 static const char USAGE_STR[] = "\n"
@@ -35,12 +36,14 @@ static const char USAGE_STR[] = "\n"
         "  -h                       : help\n"
         "  convert_byteLE_int16     : convert little endian byte stream to internal short ints\n"
         "  convert_aByte_f          : convert a signed byte stream to internal floating point\n"
-        "  convert_aUnsignedByte_f  : convert a signed byte stream to internal floating point\n";
+        "  convert_aUnsignedByte_f  : convert a signed byte stream to internal floating point\n"
+        "  shift_frequency_cc       : recenter a signal by x cycles per sample\n";
 
 static struct option longOpts[] = {
   { "convert_byteLE_int16"   , no_argument, NULL, 1 },
   { "convert_aByte_f"        , no_argument, NULL, 2 },
   { "convert_aUnsignedByte_f", no_argument, NULL, 3 },
+  { "shift_frequency_cc"     , no_argument, NULL, 4 },
   { NULL, 0, NULL, 0 }
 };
 
@@ -83,18 +86,28 @@ int main(int argc, char *argv[]) {
 
   while ((c = getopt_long(argc, new_argv, "h", longOpts, NULL)) >= 0 ) {
     switch (c) {
-      case 'h':
+      case 'h': {
         fprintf(stderr, USAGE_STR, argv[0]);
         return -2;
-      case 1:
+      }
+      case 1: {
         convert_byteLE_int16();
         break;
-      case 2:
+      }
+      case 2: {
         convert_aByte_f();
         break;
-      case 3:
+      }
+      case 3: {
         convert_aUnsignedByte_f();
         break;
+      }
+      case 4: {
+          float amount; 
+          sscanf(argv[2], "%f", &amount);
+          shift_frequency_cc(amount);
+          break;
+      }
       default:
 	return -2;
       }
---
+++ shift_frequency_cc.c
@@ -0,0 +1,60 @@
+/*
+ *      shift_frequency_cc.c -- DSP Pipe - shift the frequency of a quadature
+ *                              by amount cycles per sample
+ *
+ *      Copyright (C) 2019 
+ *          Mark Broihier
+ *
+ */
+
+/* ---------------------------------------------------------------------- */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+/* ---------------------------------------------------------------------- */
+
+int shift_frequency_cc(float amount) {
+  const int BUFFER_SIZE = 4096;
+  unsigned char c[BUFFER_SIZE];
+  float f[BUFFER_SIZE];
+  float of[BUFFER_SIZE];
+  int count;
+  float * fptr, * ofptr;
+  fcntl(STDIN_FILENO, F_SETPIPE_SZ, BUFFER_SIZE); 
+  fcntl(STDOUT_FILENO, F_SETPIPE_SZ, BUFFER_SIZE); 
+  float sinDeltaAmount = sin(amount);
+  float cosDeltaAmount = cos(amount);
+  float cosAmount = 1.0;
+  float sinAmount = 0.0;
+  float I;
+  float Q;
+  for (;;) {
+    count = fread(&f, sizeof(char), BUFFER_SIZE, stdin);
+    if(count < BUFFER_SIZE) {
+      fprintf(stderr, "Short data stream\n");
+      fclose(stdout);
+      return 0;
+    }
+    fptr = f;
+    ofptr = of;
+
+    for (int i=0; i < BUFFER_SIZE; i+=sizeof(float)*2) {
+      I = *fptr++;
+      Q = *fptr++;
+      *ofptr++ = I * cosAmount + Q * sinAmount;
+      *ofptr++ = Q * cosAmount - I * sinAmount;
+      float newCosAmount = cosAmount * cosDeltaAmount - sinAmount * sinDeltaAmount;
+      float newSinAmount = sinAmount * cosDeltaAmount + cosAmount * sinDeltaAmount;
+      cosAmount = newCosAmount;
+      sinAmount = newSinAmount;
+    }
+    fwrite(&of, sizeof(float), BUFFER_SIZE, stdout);
+  }
+
+  return 0;
+
+}
+
+/* ---------------------------------------------------------------------- */
