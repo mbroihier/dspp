871cc6e6f7cd43fb0e4e77a55ed4ebb1c6334427
Mark Broihier <mbroihier@yahoo.com>
2022-11-28 16:47:28 +0000

    add candidate concept to find largest magnitude frequency

--- FindNLargestF.cc
+++ FindNLargestF.cc
@@ -34,17 +34,23 @@ FindNLargestF::FindNLargestF(int size, int number) {
 }
 
 void FindNLargestF::doWork() {
+  std::map<int, float> candidates;    // list of cadidates mapped to their centroid location
+  std::map<int, int> candidateToGroup;  // mapping of candidate to group for this cycle
+  int numberOfCandidates = 0;
+  std::map<int, float> groupCentroids;  // mapped by group ID
   int frame = 0;
   int count = 0;
   float * samplePtr;
   float * magPtr;
   fprintf(stderr, "Find %d largest magnitude frequencies in FFT\n", number);
   bool done = false;
-  bool firstWrite = true;
   int windowS = 0;
   int windowE = 0;
   while (!done) {
+    // get an FFT's worth of bins
+    fprintf(stderr, "done with set of data\n");
     count = fread(samples, sizeof(float), sampleBufferSize, stdin);
+    fprintf(stderr, "done with read\n");
     if (count < sampleBufferSize) {
       done = true;
       continue;
@@ -66,7 +72,7 @@ void FindNLargestF::doWork() {
       bin++;
     }
     fprintf(stderr, "bin should be %d, it is %d\n", size, bin);
-    // make a sorted list of bin numbers that contain the highest frequency magnitudes
+    // make a sorted list (up to number) of bin numbers that contain the highest frequency magnitudes
     for (int binIndex = 1; binIndex < number; binIndex++) {
       float threshold = mag[binArray[binIndex - 1]];
       peak = 0.0;
@@ -86,6 +92,7 @@ void FindNLargestF::doWork() {
       }
       binArray[binIndex] = bin;
     }
+    // output this array of integer bin numbers that are the indices of the highest amplitude frequencies
     fwrite(binArray, sizeof(int), number, stdout);
     fprintf(stderr, "binArray on frame %d\n", frame++);
     for (int binIndex = 0; binIndex < number; binIndex++) {
@@ -107,7 +114,7 @@ void FindNLargestF::doWork() {
       for (int groupIndex = 0; groupIndex < numberOfGroups; groupIndex++) {
         escape = false;
         for (std::list<int>::iterator iter = groupToBins[groupIndex]->begin(); iter != groupToBins[groupIndex]->end(); iter++) {
-          if (abs(binArray[binIndex] - *iter) <= 1) { // this bin belongs to this group
+          if (abs(binArray[binIndex] - *iter) <= 2) { // this bin belongs to this group
             groupToBins[groupIndex]->push_back(binArray[binIndex]);
             binToGroup[binArray[binIndex]] = groupIndex;
             escape = true;
@@ -124,12 +131,7 @@ void FindNLargestF::doWork() {
         groupToBins[groupNumber]->push_back(binArray[binIndex]);
       }
     }
-    FILE * fh;
-    if (firstWrite) {
-      fh = fopen("grp0cent.txt", "w");
-    } else {
-      fh = fopen("grp0cent.txt", "a");
-    }      
+    // find the centroids of each group
     for (int groupIndex = 0; groupIndex < numberOfGroups; groupIndex++) {
       fprintf(stderr, "bins in group %d:", groupIndex);
       groupToBins[groupIndex]->sort();
@@ -141,27 +143,78 @@ void FindNLargestF::doWork() {
         accumulator += mag[*iter];
       }
       weightedCentroid = weightedCentroid / accumulator;
-      if (groupIndex == 0) {
-        if (firstWrite) {
-          firstWrite = false;
-          windowS = (groupToBins[groupIndex]->front() + groupToBins[groupIndex]->back())/2 - 10;
-          windowE = windowS + 21;
-          assert(windowS >=0 && windowE < size);
+      fprintf(stderr," --- weighted centroid: %f\n\n",weightedCentroid);
+      groupCentroids[groupIndex] = weightedCentroid;  // indexed by groupIndex
+    }
+    std::map<int, bool> alreadyUpdated;
+    for (int canID = 0; canID < numberOfCandidates; canID++) {
+      alreadyUpdated[canID] = false;
+    }
+    // map groups to candidates
+    for (int groupIndex = 0; groupIndex < numberOfGroups; groupIndex++) {
+      bool newCandidate = true;
+      int currentCandidateID = 0;
+      for (int canID = 0; canID < numberOfCandidates; canID++) {
+        if (fabs(groupCentroids[groupIndex] - candidates[canID]) < 5.0) {
+          if (alreadyUpdated[canID]) {
+            fprintf(stderr, "suppressing update of candidate %d with group %d info\n",
+                    canID, groupIndex);
+            break;
+          }
+          newCandidate = false;
+          candidateToGroup[canID] = groupIndex;
+          currentCandidateID = canID;
+          candidates[currentCandidateID] = groupCentroids[groupIndex];
+          fprintf(stderr, "updating candidate centroid for candidate %d with %f from group %d\n",
+                  currentCandidateID, groupCentroids[groupIndex], groupIndex);
+          alreadyUpdated[canID] = true;
+          break;
         }
+      }
+      if (newCandidate) {
+        candidates[numberOfCandidates] = groupCentroids[groupIndex];
+        candidateToGroup[numberOfCandidates] = groupIndex;
+        alreadyUpdated[numberOfCandidates] = true;
+        FILE * fh;
+        char fileName[50];
+        sprintf(fileName, "cand%d.txt", numberOfCandidates); 
+        fh = fopen(fileName, "w");  // empty file if it exists
+        fclose(fh);
+        if (numberOfCandidates <= number) {
+          numberOfCandidates++;
+        }
+        fprintf(stderr, "New candidate list\n");
+        for (std::map<int, float>::iterator iter = candidates.begin(); iter != candidates.end(); iter++) {
+          fprintf(stderr, "candidates[%d]: %f\n", (*iter).first, (*iter).second);
+        }
+      }
+    }
+    // within this FFT, output the centroids of candidates  
+    for (int candidateIndex = 0; candidateIndex < numberOfCandidates; candidateIndex++) {
+      FILE * fh;
+      char fileName[50];
+      sprintf(fileName, "cand%d.txt", candidateIndex);
+      fh = fopen(fileName, "a");
+      if (candidateIndex == 0 && alreadyUpdated[candidateIndex]) {
         char peaks[22];
         peaks[0] = 0;
         bool windowOK = false;
         for (int i = windowS; i < windowE; i++) {
-          if (groupToBins[0]->end() != std::find(groupToBins[0]->begin(), groupToBins[0]->end(), i)) {
+          if (groupToBins[candidateToGroup[candidateIndex]]->end() !=
+              std::find(groupToBins[candidateToGroup[candidateIndex]]->begin(),
+                        groupToBins[candidateToGroup[candidateIndex]]->end(), i)) {
             windowOK = true;
             break;
           }
         }
         if (! windowOK) {
-          windowS = (groupToBins[groupIndex]->front() + groupToBins[groupIndex]->back())/2 - 10;
+          windowS = (groupToBins[candidateToGroup[candidateIndex]]->front() +
+                     groupToBins[candidateToGroup[candidateIndex]]->back())/2 - 10;
           windowE = windowS + 21;
           assert(windowS >=0 && windowE < size);
-          fprintf(fh, "------- window alignment change\n");
+          fprintf(fh, "------- window alignment change - candidate 0 found in group %d - windowS %d, windowE %d\n",
+                  candidateToGroup[candidateIndex],
+                  windowS, windowE);
         }
         for (int i = windowS; i < windowE; i++) {
           if (i == windowS) {
@@ -172,7 +225,10 @@ void FindNLargestF::doWork() {
             if (i == windowE - 1) {
               strcat(peaks,"0");
             } else {
-              if (mag[i - 1] < mag[i] && mag[i + 1] < mag[i] && groupToBins[0]->end() != std::find(groupToBins[0]->begin(), groupToBins[0]->end(), i)) {
+              if (mag[i - 1] < mag[i] && mag[i + 1] < mag[i] &&
+                  groupToBins[candidateToGroup[candidateIndex]]->end() !=
+                  std::find(groupToBins[candidateToGroup[candidateIndex]]->begin(),
+                            groupToBins[candidateToGroup[candidateIndex]]->end(), i)) {
                 strcat(peaks,"1");
               } else {
                 strcat(peaks,"0");
@@ -180,15 +236,26 @@ void FindNLargestF::doWork() {
             }
           }
         }
-        fprintf(fh, ", %s, %5.2f\n", peaks, weightedCentroid);
+        if (alreadyUpdated[candidateIndex]) {
+          fprintf(fh, ", %s, %5.2f\n", peaks, candidates[candidateIndex]);
+        } else {
+          fprintf(fh, ", %s, %5.2f, not updated in this pass\n", peaks, candidates[candidateIndex]);
+        }
+      } else {
+        if (alreadyUpdated[candidateIndex]) {
+          fprintf(fh, "%5.2f\n", candidates[candidateIndex]);
+        } else {
+          fprintf(fh, "%5.2f, not updated in this pass\n", candidates[candidateIndex]);
+        }
       }
-      fprintf(stderr," --- weighted centroid: %f\n\n",weightedCentroid);
+      fclose(fh);
     }
-    fclose(fh);
   }
+  fprintf(stderr, "leaving doWork within FindNLargestF\n");
 }
 
 FindNLargestF::~FindNLargestF(void) {
+  fprintf(stderr, "destructing FindNLargestF\n");
   if (mag) free(mag);
   if (binArray) free(binArray);
   if (samples) free(samples);
