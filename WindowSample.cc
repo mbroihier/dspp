3eff57e31ab8d0586f6eed15693e6e5705a066eb
Mark Broihier <mbroihier@yahoo.com>
2023-04-26 18:44:52 +0000

    simplify logic and always pass real samples/don't zero

--- WindowSample.cc
+++ WindowSample.cc
@@ -22,11 +22,8 @@ WindowSample::WindowSample(int samplesInWindow, int samplesInPeriod, int syncTo)
   fprintf(stderr, "creating WindowSample object\n");
   this->samplesInWindow = samplesInWindow;
   this->samplesInPeriod = samplesInPeriod;
-  fillerBufferSize = samplesInPeriod - samplesInWindow;
   this->syncTo = syncTo;
-  filler = reinterpret_cast<uint8_t *>(malloc(samplesInPeriod));
-  windowOfIQSamples = reinterpret_cast<uint8_t *>(malloc(samplesInWindow));
-  memset(filler, 0x80, samplesInPeriod);
+  windowOfIQSamples = reinterpret_cast<uint8_t *>(malloc(2));
   fprintf(stderr, "done creating WindowSample object\n");
   
 }
@@ -37,37 +34,30 @@ void WindowSample::doWork() {
   int count = 0;
   bool done = false;
   time_t now = 0;
+  if (currentState == 0) { // if it is currently at the sync, we need to skip this window
+    sleep(2.0);
+  }
+  while (0 != (currentState = time(0) % syncTo)) {  // wait here until time to sample
+    fread(windowOfIQSamples, sizeof(uint8_t), 2, stdin);  // skip samples
+  }
   while (!done) {
-    if (currentState == 0) { // if it is currently at the sync, we need to skip this window
-      sleep(2.0);
-    }
-    while (0 != (currentState = time(0) % syncTo)) {  // wait here until time to sample
-      uint8_t skipSamples[2];
-      fread(skipSamples, sizeof(uint8_t), 2, stdin);  // skip samples
-    }
     now = time(0);
     fprintf(stderr, "taking samples @ %s", ctime(&now));
     accumulator = 0;
     count = -1;
-    while (accumulator != samplesInWindow && count != 0) {
+    while (accumulator != samplesInPeriod && count != 0) {
       count = fread(windowOfIQSamples, sizeof(uint8_t), 2, stdin);
       fwrite(windowOfIQSamples, sizeof(uint8_t), 2, stdout);
       accumulator += count;
     }
-    done =  (accumulator != samplesInWindow);
-    if (!done) {
-      fwrite(filler, sizeof(uint8_t), fillerBufferSize, stdout);
-      currentState = time(0) % syncTo;
-    }
-    now = time(0);
-    fprintf(stderr, "done @ %s", ctime(&now));
+    done =  (accumulator != samplesInPeriod);
+    fprintf(stderr, "window done\n");
   }
   fprintf(stderr, "leaving doWork within WindowSample\n");
 }
 
 WindowSample::~WindowSample(void) {
   fprintf(stderr, "destructing WindowSample\n");
-  if(filler) free(filler);
   if(windowOfIQSamples) free(windowOfIQSamples);
 }
 
