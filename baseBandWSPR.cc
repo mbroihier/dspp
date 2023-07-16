2dc5859df25cfe2b51b33b08f3c79b5ef5211f51
Mark Broihier <mbroihier@yahoo.com>
2023-07-16 20:15:53 +0000

    added testing tool

---
+++ baseBandWSPR.cc
@@ -0,0 +1,75 @@
+/*
+ *      base band WSPR signal - generate WSPR signal to test WSPRWindow vs
+ *                            rtlsdr_wsprd
+ *
+ *      Copyright (C) 2023
+ *          Mark Broihier
+ *
+ */
+
+/* ---------------------------------------------------------------------- */
+#include <complex.h>
+#include <stdio.h>
+/* ---------------------------------------------------------------------- */
+int main(int argc, char *argv[]) {
+
+  // the vector below should result in a call sign of KG5YJE, a location of EM13 and power of 10
+  int testInput[] = {3, 3, 2, 2, 2, 2, 2, 2, 3, 0, 2, 0, 3, 1, 1, 0, 0, 0, 1, 2, 2, 1, 2, 1, 1, 1, 3, 0, 0, 2,
+                     2, 2, 2, 2, 1, 0, 2, 3, 0, 1, 0, 0, 0, 0, 0, 2, 1, 0, 3, 3, 2, 0, 1, 3, 2, 3, 2, 2, 2, 3,
+                     3, 0, 3, 0, 0, 2, 2, 3, 1, 0, 1, 2, 3, 0, 3, 2, 3, 2, 0, 1, 0, 0, 1, 2, 1, 1, 2, 0, 0, 3,
+                     3, 0, 1, 2, 1, 2, 2, 2, 1, 0, 0, 0, 0, 2, 3, 0, 0, 3, 0, 0, 1, 3, 1, 2, 3, 3, 0, 2, 1, 3,
+                     0, 1, 0, 0, 2, 3, 1, 1, 2, 2, 2, 0, 0, 3, 2, 1, 0, 0, 1, 3, 2, 0, 2, 2, 0, 0, 0, 1, 1, 2,
+                     3, 0, 3, 1, 2, 0, 0, 3, 3, 2, 0, 2};
+  const int SAMPLING_FREQUENCY = 375;  // Hz
+  double t = 0.0;  // seconds
+  const double deltaT = 1.0 / SAMPLING_FREQUENCY;  // seconds
+  const int DURATION = 120;
+  std::complex<double> signal;
+  int decodedSignalAtT[DURATION * SAMPLING_FREQUENCY] = {0};
+  int startSignalAt = 0.5 * SAMPLING_FREQUENCY;
+  int index = 0;
+  for (auto token : testInput) {
+    for (int sym = 0; sym < 256; sym++) {
+      decodedSignalAtT[startSignalAt + index] = token;
+      index++;
+    }
+  }
+  const float DEVIATION = 1.4648; // Hz
+
+  float offset = 50.0;
+  float toneGain = 0.5;
+  float driftRate = 0.0;  // Hz/min
+  float driftDelta = 0.0;
+  if (argc == 4) {
+    sscanf(argv[1], "%f", &offset);
+    sscanf(argv[2], "%f", &driftRate);
+    sscanf(argv[3], "%f", &toneGain);
+    driftDelta = driftRate / 60.0 * SAMPLING_FREQUENCY / 256.0; // Hz/sample
+  }
+  std::complex<double> omega0 = 2 * M_PI * (-DEVIATION*3/2 + offset) * I;
+  std::complex<double> omega1 = 2 * M_PI * (-DEVIATION/2 + offset) * I;
+  std::complex<double> omega2 = 2 * M_PI * (DEVIATION/2 + offset) * I;
+  std::complex<double> omega3 = 2 * M_PI * (DEVIATION*3/2 + offset) * I;
+  fprintf(stderr, "signal offset: %f, drift rate: %2.1f, tone gain %5.2f\n", offset, driftRate, toneGain);
+  index = 0;
+  float realPart;
+  float imagPart;
+  std::complex<double> toneGainC = std::complex<double>(toneGain + 0.0 * I);
+  while (t < DURATION) {
+    signal = toneGainC * (((decodedSignalAtT[index] == 0) * 1.0) * exp(omega0 * t) +
+                                 ((decodedSignalAtT[index] == 1) * 1.0) * exp(omega1 * t) +
+                                 ((decodedSignalAtT[index] == 2) * 1.0) * exp(omega2 * t) +
+                                 ((decodedSignalAtT[index] == 3) * 1.0) * exp(omega3 * t)); 
+
+    realPart = real(signal);
+    imagPart = imag(signal);
+    fwrite(&realPart, sizeof(float), 1, stdout);
+    fwrite(&imagPart, sizeof(float), 1, stdout);
+    t += deltaT;
+    index++;
+  }
+  return 0;
+
+}
+
+/* ---------------------------------------------------------------------- */
